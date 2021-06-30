ab2833bdd94fd68e7eb9804165e50d37d8ac8dfb
Anthony Hollis <anthonyrayhollis@gmail.com>
2021-06-30 12:22:03 -0400

    Update main.cpp

--- BareMetalProgramming/DAC/src/main.cpp
+++ BareMetalProgramming/DAC/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // create triangle waveform
+  for(int value = 0; value < 255; value++)
+  {
+    dacWrite(25, value);
+  }
+
+  for (int value = 255;  value > -1; value--)
+  {
+    dacWrite(25, value);
+  }
+
 }
